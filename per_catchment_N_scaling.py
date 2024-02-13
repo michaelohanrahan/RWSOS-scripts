d843b06cd75785d47ea1ab7a25725f208b61cfdc
Michael O'Hanrahan <104068149+michaelohanrahan@users.noreply.github.com>
2024-02-13 09:42:19 +0100

    Interreg working again, add scripting for per catchment scaling

--- per_catchment_N_scaling.py
+++ per_catchment_N_scaling.py
@@ -1,37 +1,132 @@
 from hydromt_wflow import WflowModel
 from model_building.update_gauges import main as update_gauges
 from file_methods.postprocess import find_model_dirs, find_toml_files
+import geopandas as gpd
 import os
+import pandas as pd
 
 working_dir = r"P:\11209265-grade2023\wflow\wflow_meuse_julia\wflow_meuse_per_catchment_N"
-models = find_model_dirs(working_dir, 'fl1d')
-tomls = find_toml_files(models)
+model_snippet = 'fl1d'
+csv_dir = r"P:\11209265-grade2023\wflow\wflow_meuse_julia\wflow_meuse_per_catchment_N\wflow_id_to_plot.csv"
 
-print(models)
-print(tomls)
-
-# Initialize the model
-model = WflowModel(root=models[0], config_fn=tomls[1], mode='r')
-
-if 'staticgeoms' in os.listdir(models[0]):
-    static_geoms = os.path.join(models[0], 'staticgeoms')
-    files_list = os.listdir(static_geoms)
+def list_dirs_containing(root:str, snippet:str)->list:
+    """
+    List all directories containing a given snippet.
     
-    gauges_append = []
+    Parameters:
+    root (str): The path to the root directory.
+    snippet (str): The snippet to search for.
     
-    for file in files_list:
-        if 'gauges' in file:
-            gauges_append.append(file)
+    Returns:
+    list: A list of directories containing the given snippet.
+    """
+    
+    dirs = [os.path.join(root, dir) for root in roots for dir in os.listdir(root) if snippet in dir]
+    
+    return dirs
 
-gauges_files = [os.path.join(static_geoms, file) for file in gauges_append]
+def find_staticgeoms(working_dir:str, model_snippet:str = 'fl1d')->list:
+    """
+    Find the staticgeoms directory in the working directory.
+    
+    Parameters:
+    working_dir (str): The path to the working directory.
+    model_snippet (str, optional): A snippet to filter model directories. Defaults to 'fl1d'.
+    
+    Returns:
+    list: A list of staticgeoms directories associated with the given working directory.
+    """
+    
+    models = find_model_dirs(working_dir, model_snippet)
+    main_folder_sg = [os.path.join(working_dir, 'staticgeoms')]
+    
+    if os.path.exists(main_folder_sg):
+        return main_folder_sg
+    
+    staticgeoms_dir = [os.path.join(model, 'staticgeoms') for model in models]
+    staticgeoms_dir = [dir for dir in staticgeoms_dir if os.path.exists(dir)]
+    print('staticgeoms_dir:', staticgeoms_dir)
+    return staticgeoms_dir
 
+def process_gauges(working_dir:str,
+                   csv_dir:str,
+                   model_snippet:str = 'fl1d', 
+                   check_locs:bool = False)->list:
+    """
+    Process gauges associated with a given working directory and CSV directory.
+    
+    Parameters:
+    working_dir (str): The path to the working directory.
+    csv_dir (str): The path to the CSV directory.
+    model_snippet (str, optional): A snippet to filter model directories. Defaults to 'fl1d'.
+    
+    Returns:
+    list: A list of gauge files associated with the given working directory.
+    """
+    
+    models = find_model_dirs(working_dir, model_snippet)
+    tomls = find_toml_files(models)
+    wflow_to_id = pd.read_csv(csv_dir,index_col='wflow_id')
+    wflow_id = list(wflow_to_id.index.values)
+    print('models', models)
+    staticgeoms_dir = find_staticgeoms(models, model_snippet)
+    print(staticgeoms_dir)
+    gauges_append = list_dirs_containing(staticgeoms_dir, 'gauges')
+    gauges_files = [os.path.join(staticgeoms_dir, file) for file in gauges_append]
 
-# Load the default configuration
-model.read()
-model.config
+    wflow_gauge_data = []
+    best_match_file_names = []
+    
+    for i, gauge in enumerate(gauges_files):
+        file = gpd.read_file(gauge)
+        if 'wflow_id' in file.columns:
+            # Count how many of the wflow id's are in the file
+            print(f'The gauges in the file {gauges_append[i]} have wflow ids:\n{file.wflow_id.isin(wflow_id).sum()}/{len(wflow_id)}')
+            if file.wflow_id.isin(wflow_id).sum() == len(wflow_id):
+                file.set_index('wflow_id', inplace=True)
+                wflow_gauge_data.append(file)
+                best_match_file_names.append(gauges_append[i])
 
+    if len(gauges_files) > 0 and check_locs == True: 
+        #perform pairwise comparison of point locations associated with wflow_id
+        for id in wflow_id:
+            points = []
+            for file in wflow_gauge_data:
+                # file.set_index('wflow_id', inplace=True)
+                #get the geom at location of wflow_id
+                point = file.loc[id].geometry
+                points.append(point)
+            #compare the points
+            if points[0] == points[1]:
+                print(f'wflow_id {id} has the same location in both files')
+                
+            else:
+                print(f'wflow_id {id} has DIFFERENT locations')
+                
+    match_dict = {file:data for file,data in zip(best_match_file_names,wflow_gauge_data)}
+    
+    return match_dict, models, tomls, wflow_id, staticgeoms_dir
 
-# #load the hourly gauges
-# new_gdf = gpd.read_file(hourly_gauges_path)
+match_dict, models, tomls, wflow_id, staticgeoms_dir = process_gauges(working_dir, csv_dir, model_snippet, check_locs=False)      
 
-# update_gauges
+dependency_dict = {}
+
+for id in wflow_id:
+    dependency_dict[id] = []
+
+low_dependency = [101, 201, 6, 9, 701, 801, 11, 10, 12, 13,15,703]
+
+for i in low_dependency:
+    dependency_dict[i] = []
+
+dependency_dict[3] = [101]
+dependency_dict[4] = [*dependency_dict[3], 201, 5, 6]
+dependency_dict[1401] = [13,9,703,701,*dependency_dict[4], 801]
+dependency_dict[1201] = [12,11,10]
+dependency_dict[1402] = [*dependency_dict[1401], *dependency_dict[1201]]
+dependency_dict[16] = [15, *dependency_dict[1402]]
+
+for key, item in dependency_dict.items():
+    print(f'wflow_id {key} has dependencies (n={len(item)}) {item}')
+    
+
---
+++ plot_hourly_runs_interreg_lowN.py
@@ -0,0 +1,262 @@
+#%%
+#========================== imports ==========================
+#env: RWSOS_environment.yml
+#title: Plotting hourly model runs
+
+import xarray as xr
+import numpy as np
+import pandas as pd
+from datetime import datetime
+# from tqdm import tqdm
+import matplotlib.pyplot as plt
+import sys
+import os
+# from hydro_plotting import hydro_signatures
+from file_methods.postprocess import find_model_dirs, find_toml_files, find_outputs, create_combined_hourly_dataset_FRBENL
+# from metrics.peak_metrics import peak_timing_errors
+from metrics.run_peak_metrics import store_peak_info
+from hydro_plotting.peak_timing import plot_peaks_ts, peak_timing_for_runs
+
+#TODO: move to plotting_methods
+#======================= Plot hydrograph =================================================================
+def plot_ts(ds:xr.Dataset, 
+            scales:list, 
+            df_GaugeToPlot:pd.DataFrame, 
+            start:str, 
+            end:str, 
+            Folder_plots:str, 
+            action:str, 
+            var:str,
+            color_dict:dict,
+            peak_time_lag:bool=False, 
+            savefig:bool=False, 
+            font:dict={'family': 'serif', 'size': 16}
+            )->None:
+    """
+    ds: xarray dataset that contains modeled results for all runs and observation data
+    scales: list of scaling factors, eg ['0.7', '0.8', '0.9', '1.0', '1.1', '1.2']
+    df_GaugeToPlot: pandas dataframe that contains the information about the wflow_id to plot,
+                    see for instance: p:\11209265-grade2023\wflow\wflow_meuse_julia\wflow_meuse_202312\wflow_id_to_plot.csv
+    start: start date to plot, eg '2015-01-01'
+    end: end date to plot, eg '2015-12-31'
+    Folder_plots: directory to save figures
+    savefig: save the figures or not
+    Peak_time_lag: calculate the peak timing lag or not, and present in legend, only useful in the case of single events
+    action: The method, for the saved figure name e.g. scaling, offsetting, etc.
+    var: The variable, for the saved figure name e.g. riverN, that is being scaled or offsetted.
+    """
+    # Define the scales for the legend
+    translate = {f's{scale.replace(".", "")}': f'scale: {scale}' for scale in scales}
+    translate['Obs.'] = 'Observed'
+    
+    for id in ds.wflow_id.values:
+        station_name = df_GaugeToPlot.loc[df_GaugeToPlot['wflow_id']==id, 'station_name'].values[0]
+        station_id = id
+        
+        try:
+            
+            fig, ax = plt.subplots(figsize=(12,6.18))
+            ax.set_title(f'{station_name} (id: {station_id})', fontdict=font)
+            
+            obs_max_time = ds.sel(time=slice(start, end), runs='Obs.', wflow_id=id).Q.idxmax().values
+
+            for run in ds.runs.values:
+                if str(run) in ['s07', 's08','s09', 's10', 's11', 's12', 'Obs.']:
+                    # Select the specific run and station from ds
+                    subset = ds.sel(time=slice(start, end),
+                                    runs=run, 
+                                    wflow_id=id).dropna(dim='time')  
+                    # print('subset', subset)
+                    # Get the time index of the maximum value in this run
+                    run_max_time = subset.sel(time=slice(obs_max_time - pd.Timedelta(hours=72), obs_max_time + pd.Timedelta(hours=72))).Q.idxmax().values
+                    # Calculate the difference in peak timing
+                    dt = run_max_time - obs_max_time
+                    dt_hours = dt.astype('timedelta64[h]').item().total_seconds() / 3600
+                    # Set the y-axis label
+                    ax.set_ylabel('Discharge ($m^3s^{-1}$)')
+                    # Set the font properties for the y-axis labels
+                    ax.tick_params(axis='y', labelsize=font['size'])
+                    # Set the x-axis label
+                    ax.set_xlabel('Date (hourly timestep)')
+                    # Set the font properties for the x-axis labels
+                    ax.tick_params(axis='x', labelsize=font['size'])
+                    # Set labels
+                    if run == 'Obs.':
+                        label = f'{translate[run]}'
+                    elif peak_time_lag==True:
+                        label = f'{translate[run]}, model lag = {dt_hours:.2f} hours'
+                    else:
+                        label = f'{translate[run]}'
+                    
+                    # Plot the subset for this run
+                    ax.plot(subset.time, subset.Q, label=label, c=color_dict[str(run)])
+                
+                else:
+                    continue
+
+            # Add the legend outside of the loop
+            ax.legend()
+            plt.tight_layout()
+            # Set the x-axis limits to the time slice
+            start = pd.to_datetime(start)
+            end = pd.to_datetime(end)
+            ax.set_xlim([start - pd.Timedelta(hours=48), end + pd.Timedelta(hours=48)])
+            ax.grid()
+            
+            
+            # save plots
+            if savefig == True:
+                # # Create the directory if it doesn't exist
+                print('saving...')
+                plots_dir = os.path.join(working_folder, '_plots')
+                os.makedirs(plots_dir, exist_ok=True)
+                filename = f'timeseries_{station_name}_{station_id}_{start.year}{start.month}{start.day}-{end.year}{end.month}{end.day}_{action}_{var}.jpg'
+                # Save the figure
+                fig.savefig(os.path.join(Folder_plots, filename), dpi=300)
+                # print(f'saved to {timeseries_{station_name}_{station_id}_{start.month, start.day}_{end.month,end.day}.png}')
+            else:
+                pass
+        except Exception as e:
+            print('fail timeseries:', station_id)
+            print(e)
+            pass
+
+#%%
+# ======================= Set up the working directory =======================
+working_folder = r"P:\11209265-grade2023\wflow\wflow_meuse_julia\wflow_meuse_20240125_riverN"
+sys.path.append(working_folder)
+
+# ======================= Define the runs and load model runs =======================
+snippets = ['run_scale']
+model_dirs = find_model_dirs(working_folder, snippets)
+toml_files = find_toml_files(model_dirs)  #will be useful when we can use the Wflowmodel to extract geoms and results
+
+#TODO: make this more general with a list of model paths
+run_keys = [run.split('\\')[-1].split('_')[-1] for run in model_dirs]
+
+# ======================= Create the FR-BE-NL combined dataset =======================  
+ds, df_gaugetoplot = create_combined_hourly_dataset_FRBENL(working_folder, 
+                                                           run_keys, 
+                                                           model_dirs, 
+                                                           output='csv',
+                                                           toml_files= toml_files, 
+                                                           overwrite=True)
+
+print(f'Loaded dataset with dimensions: {ds.dims}')
+
+#%%
+#======================== Create Plotting Constants =======================
+#TODO: automate the color list, make permanent for each working folder? 
+
+color_list = ['#377eb8', '#ff7f00', '#4daf4a', '#f781bf', '#a65628', '#984ea3', '#999999'] #blue, orange, green,pink,brown,purple,grey
+
+run_keys = ds.runs.values
+
+color_dict = {f'{key}': color_list[i] for i, key in enumerate(run_keys)}
+
+start = datetime.strptime('2015-01-01', '%Y-%m-%d')
+
+end = datetime.strptime('2018-02-21', '%Y-%m-%d')
+
+peak_dict = store_peak_info(ds.sel(time=slice(start,end)), df_gaugetoplot, 'wflow_id', 72)
+
+#%%
+# # ======================= Plot Peak Timing Hydrograph =======================
+# plot and store peak timing information in a dictionary ''peak timing info'
+# dict is indexed by (key: station id), then by (key: run name), then a tuple of (0: obs indices, 1:sim indices and 2:timing errors)
+Folder_plots = os.path.join(working_folder, '_plots')  # folder to save plots
+
+print(f'len ds time: {len(ds.time)}')
+
+peak_timing_info = plot_peaks_ts(ds, 
+                                 df_gaugetoplot,
+                                 start, end,
+                                 Folder_plots,
+                                 color_dict,
+                                 peak_dict=peak_dict,
+                                 savefig=True)
+
+
+
+
+# whole TS
+# plot_ts(ds, scales, df_GaugeToPlot, start, end, Folder_plots, peak_time_lag=False, savefig=False)
+
+
+# plot_ts(ds, 
+#         run_keys, 
+#         df_GaugeToPlot, 
+#         start, end, 
+#         Folder_plots, 
+#         action='scaling',
+#         var='riverN',
+#         peak_time_lag=False, 
+#         savefig=True,
+#         color_dict=color_dict, 
+#         font=font) 
+
+# # # peak event Jan-Feb
+# # plot_ts(ds, df_GaugeToPlot, '2015-01-01', '2015-02-14', Folder_plots, peak_time_lag=True, savefig=False)
+# # # peak event Feb-Mar
+# # plot_ts(ds, df_GaugeToPlot, '2015-02-15', '2015-03-31', Folder_plots, peak_time_lag=True, savefig=False)
+# # # peak event Apr-May
+# # plot_ts(ds, df_GaugeToPlot, '2015-04-01', '2015-05-14', Folder_plots, peak_time_lag=True, savefig=False)
+# # # peak event Sep-Oct
+# # plot_ts(ds, df_GaugeToPlot, '2015-09-01', '2015-10-14', Folder_plots, peak_time_lag=True, savefig=False)
+# # # peak event Nov-Dec
+# # plot_ts(ds, df_GaugeToPlot, '2015-11-15', '2015-12-31', Folder_plots, peak_time_lag=True, savefig=False)
+
+
+# # ======================= Plot signature =======================
+# colors = [
+#     '#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c',
+#     '#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928']
+# plot_colors = colors[:len(model_runs)]
+# translate = {
+#         's07': 'scale: 0.7',
+#         's08': 'scale: 0.8',
+#         's09': 'scale: 0.9',
+#         's11': 'scale: 1.1',
+#         's12': 'scale: 1.2',
+#         's10': 'scale: 1.0',
+#         'Obs.': 'Observed',
+#         }
+
+# for id in ds.wflow_id.values:
+#     station_name = df_GaugeToPlot.loc[df_GaugeToPlot['wflow_id']==id, 'station_name'].values[0]
+#     station_id = id
+    
+#     dsq = ds.sel(wflow_id=id)
+#     # dsq.sel(time=dsq.time[~dsq.Q.sel(runs="Obs.").isnull()].values)
+        
+#     try:
+#         plot_signatures(
+#             dsq=dsq, labels=list(model_runs.keys()), colors=plot_colors,
+#             Folder_out=Folder_plots, station_name=station_name, station_id=station_id, save=False,
+#             )
+            
+#     except Exception as e:
+#         print(e)
+#         pass
+
+
+# ======================= peak timing errors =======================
+# set figure fonts
+SMALL_SIZE = 12
+MEDIUM_SIZE = 14
+BIGGER_SIZE = 16
+plt.rc('font', size=MEDIUM_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=MEDIUM_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=MEDIUM_SIZE)  # fontsize of the figure title
+
+peak_timing_for_runs(ds, 
+                     df_gaugetoplot, 
+                     Folder_plots, 
+                     peak_dict=peak_dict,
+                     plotfig=True, 
+                     savefig=True)
+# %%
